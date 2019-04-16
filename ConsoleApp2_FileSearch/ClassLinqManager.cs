0f57cb116bdd3b9b3a47fec79450913a1e34280d
Agure <46519600+AgureDa2019@users.noreply.github.com>
2019-04-17 00:02:27 +0900

    初回アップロード

---
+++ ConsoleApp2_FileSearch/ClassLinqManager.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+//using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+//using System.Text;
+
+namespace ConsoleApp2_FileSearch
+{
+    class ClassLinqManager
+    {
+
+        /// <summary>
+        /// 対象フォルダ配下にある特定ファイルを検索
+        /// </summary>
+        /// <param name="dirPath">対象フォルダのパス</param>
+        /// <param name="extension">ファイル拡張子</param>
+        /// <param name="keyword">ファイル名に含まれる検索キーワード</param>
+        /// <param name="errMsg">エラーメッセージ</param>
+        /// <returns></returns>
+        public static string SearchFilePath(string dirPath , string extension, string keyword, out string errMsg)
+        {
+            string getPath = "";
+
+            errMsg = "";
+            try
+            {
+                // LINQ query for all .txt files containing the word ＜keyword＞.
+                var targetFile = (from file in Directory.EnumerateFiles(@dirPath, extension)
+                                  where file.Contains(keyword)
+                                  select file).FirstOrDefault();
+                if (targetFile != null)
+                {
+                    getPath = targetFile.ToString();
+                }
+
+            }catch(Exception ex)
+            {
+                getPath = "";
+                errMsg = ex.Message;
+            }
+
+            return getPath;
+
+        }
+
+    }
+}
---
+++ ConsoleApp2_FileSearch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_FileSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //メッセージ出力用
+            StringBuilder infoMsg = new StringBuilder(100);
+
+            //検索条件
+            // string desktop = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); //デスクトップフォルダ
+            string targetDir = "." + "\\test\\";
+            string fileExtension = "*.csv";
+            string fileKeyword = "テスト";
+
+            string errMsg = "";
+
+            //説明文を作成
+            infoMsg.AppendLine("以下の条件で、ファイルを検索します。");
+            infoMsg.AppendLine(" １）対象フォルダ：" + targetDir);
+            infoMsg.AppendLine(" ２）ファイル名のあいまい検索：" + fileKeyword);
+            infoMsg.AppendLine(" ３）ファイル拡張子：" + fileExtension);
+
+            //説明文を表示
+            Console.WriteLine(infoMsg.ToString());
+
+            Console.WriteLine("実行するには何かキーを押してください．．．");
+            Console.ReadKey();
+
+            //ファイルを検索
+            string getCsvPath = ClassLinqManager.SearchFilePath(targetDir, fileExtension, fileKeyword, out errMsg);
+
+            //検索結果を表示
+            Console.WriteLine("");
+            if (getCsvPath.Length > 0)
+            {
+                Console.WriteLine("検索結果：" + getCsvPath);
+            }
+            else if (errMsg.Length > 0)
+            {
+                Console.WriteLine("検索結果（エラー）：" + errMsg);
+            }
+            else
+            {
+                Console.WriteLine("検索結果：ヒットしませんでした。");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("終了するには何かキーを押してください．．．");
+            Console.ReadKey();
+
+        }
+    }
+}
